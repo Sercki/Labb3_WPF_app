b1338b1279af6019e1a8efdc085e0b04bd645448
Sergiusz Radecki <sergiusz.radecki@gmail.com>
2022-10-24 22:42:17 +0200

    Added DatePicker, added possibility to fill all of required information in respective places

--- Labb3_WPF_app/MainWindow.xaml.cs
+++ Labb3_WPF_app/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,43 @@ namespace Labb3_WPF_app
         public MainWindow()
         {
             InitializeComponent();
+            
+            datum.BlackoutDates.AddDatesInPast();
+            datum.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(30), DateTime.MaxValue));
+        }
+        public void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string[] workingday = new string[9];
+            string[] weekend = new string[13];
+            int number = 0;
+            var picker = sender as DatePicker;
+            var date = picker.SelectedDate.Value.DayOfWeek.ToString();
+            if (date == "Saturday" || date == "Sunday")
+            {                
+                DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
+                DateTime endDate = new DateTime(2022, 01, 02, 00, 00, 00);
+
+                for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
+                {
+                    weekend[number] = dtm.ToString("HH:mm");
+                    number++;
+                }
+                Tid.ItemsSource = weekend;
+
+
+            }
+            else
+            {
+                DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
+                DateTime endDate = new DateTime(2022, 01, 01, 22, 00, 00);
+
+                for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
+                {
+                    workingday[number] = dtm.ToString("HH:mm");
+                    number++;
+                }
+                Tid.ItemsSource = workingday;
+            }
         }
     }
 }
