e92313bc82ffddf1a5dbe4feee1a028c782d090f
Sergiusz Radecki <sergiusz.radecki@gmail.com>
2022-10-29 15:43:02 +0200

    Added randomly generated bookings.

---
+++ Labb3_WPF_app/HelpMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Labb3_WPF_app
+{
+    internal static class HelpMethods
+    {
+        public static void InstertToList(List<BookingInfo> inData, BookingInfo customer)
+        {
+            bool checkIfAvailable = true;
+            if (inData.Count > 0)
+            {
+                foreach (var customerBookingInfo in inData)
+                {
+                    if (customer.Date == customerBookingInfo.Date)
+                    {
+                        if (customer.Time == customerBookingInfo.Time)
+                        {
+                            if (customer.TableNumber == customerBookingInfo.TableNumber)
+                            {
+                                MessageBox.Show($"Tyvärr bordet nummer {customer.TableNumber} är redan bokad. Försök boka ett annat bord eller välja annan datum!!", "Fullbokad bord", MessageBoxButton.OK, MessageBoxImage.Error);
+                                checkIfAvailable = false;
+                                break;                                      //utan break programmet sparar ett nytt objekt av bookinInfo om det finns mer bord som är bokad på samma tid, t.ex. programmet felaktigt sparar bord nr 1 om det finns redan bokning till kl 18 för bord nr 1 och 2. "break;" skyddar mot liknande händelser.
+                            }
+                            else
+                            {
+                                checkIfAvailable = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (checkIfAvailable == true)
+            {
+                inData.Add(customer);
+                DisplayContent();
+                MessageBox.Show($"Bokning är klart. Kundnamn är: {customer.Name}", "Bekräftelse", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
--- Labb3_WPF_app/MainWindow.xaml.cs
+++ Labb3_WPF_app/MainWindow.xaml.cs
@@ -28,28 +28,42 @@ namespace Labb3_WPF_app
         public MainWindow()
         {
             InitializeComponent();
+            StartExamplesRandom();
+            BokaEnabled();
             DisplayContent();
 
-            Datum.BlackoutDates.AddDatesInPast();
-            Datum.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(30), DateTime.MaxValue));
+            PickADay.BlackoutDates.AddDatesInPast();
+            PickADay.BlackoutDates.Add(new CalendarDateRange(DateTime.Now.AddDays(30), DateTime.MaxValue));
         }
-        public void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        public void PickADay_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
+
             List<string> workingday = new List<string>();
             List<string> weekend = new List<string>();
             var picker = sender as DatePicker;
-            var date = picker.SelectedDate.Value.DayOfWeek.ToString();
-            if (date == "Saturday" || date == "Sunday")
+            if (PickADay.SelectedDate != null)
             {
-                DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
-                DateTime endDate = new DateTime(2022, 01, 02, 00, 00, 00);
-                for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
+                var date = picker.SelectedDate.Value.DayOfWeek.ToString();
+                if (date == "Saturday" || date == "Sunday")
                 {
-                    if (picker.SelectedDate == DateTime.Today)
+                    DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
+                    DateTime endDate = new DateTime(2022, 01, 02, 00, 00, 00);
+                    for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
                     {
-                        if (dtm.Hour == DateTime.Now.Hour)
+                        if (picker.SelectedDate == DateTime.Today)
                         {
-                            if (dtm.Minute > DateTime.Now.Minute)
+                            if (dtm.Hour == DateTime.Now.Hour)
+                            {
+                                if (dtm.Minute > DateTime.Now.Minute)
+                                {
+                                    weekend.Add(dtm.ToString("HH:mm"));
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else if (dtm.Hour > DateTime.Now.Hour)
                             {
                                 weekend.Add(dtm.ToString("HH:mm"));
                             }
@@ -58,34 +72,33 @@ namespace Labb3_WPF_app
                                 continue;
                             }
                         }
-                        else if (dtm.Hour > DateTime.Now.Hour)
+                        else
                         {
                             weekend.Add(dtm.ToString("HH:mm"));
                         }
-                        else
-                        {
-                            continue;
-                        }
                     }
-                    else
-                    {
-                        weekend.Add(dtm.ToString("HH:mm"));
-                    }
+                    TimeComboBox.ItemsSource = weekend;
                 }
-
-                Tid.ItemsSource = weekend;
-            }
-            else
-            {
-                DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
-                DateTime endDate = new DateTime(2022, 01, 01, 22, 00, 00);
-                for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
+                else
                 {
-                    if (picker.SelectedDate == DateTime.Today)
+                    DateTime startDate = new DateTime(2022, 01, 01, 18, 00, 00);
+                    DateTime endDate = new DateTime(2022, 01, 01, 22, 00, 00);
+                    for (DateTime dtm = startDate; dtm <= endDate; dtm = dtm.AddMinutes(30))
                     {
-                        if (dtm.Hour == DateTime.Now.Hour)
+                        if (picker.SelectedDate == DateTime.Today)
                         {
-                            if (dtm.Minute > DateTime.Now.Minute)
+                            if (dtm.Hour == DateTime.Now.Hour)
+                            {
+                                if (dtm.Minute > DateTime.Now.Minute)
+                                {
+                                    workingday.Add(dtm.ToString("HH:mm"));
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else if (dtm.Hour > DateTime.Now.Hour)
                             {
                                 workingday.Add(dtm.ToString("HH:mm"));
                             }
@@ -94,44 +107,143 @@ namespace Labb3_WPF_app
                                 continue;
                             }
                         }
-                        else if (dtm.Hour > DateTime.Now.Hour)
+                        else
                         {
                             workingday.Add(dtm.ToString("HH:mm"));
                         }
-                        else
-                        {
-                            continue;
-                        }
                     }
-                    else
-                    {
-                        workingday.Add(dtm.ToString("HH:mm"));
-                    }
+                    TimeComboBox.ItemsSource = workingday;
                 }
-                Tid.ItemsSource = workingday;
+            }
+            else
+            {
+                BokaEnabled();
             }
         }
 
         private void Boka_Click(object sender, RoutedEventArgs e)
         {
-            var customer = new BookingInfo(Datum.Text, Tid.Text, Bordsnummer.Text, Namn.Text);
-            history.Add(customer);            
-            DisplayContent();            
-            MessageBox.Show($"Booking confirmed! {customer.Date} {customer.Time}  {customer.TableNumber} {customer.Name} ");
+            var customer = new BookingInfo(PickADay.Text, TimeComboBox.Text, TableComboBox.Text, NameTextBox.Text);
+            bool checkIfAvailable = true;
+            if (history.Count > 0)
+            {
+                foreach (var customerBookingInfo in history)
+                {
+                    if (customer.Date == customerBookingInfo.Date)
+                    {
+                        if (customer.Time == customerBookingInfo.Time)
+                        {
+                            if (customer.TableNumber == customerBookingInfo.TableNumber)
+                            {
+                                MessageBox.Show($"Tyvärr bordet nummer {TableComboBox.Text} är redan bokad. Försök boka ett annat bord eller välja annan datum!!", "Fullbokad bord", MessageBoxButton.OK, MessageBoxImage.Error);
+                                checkIfAvailable = false;
+                                break;                                      //utan break programmet sparar ett nytt objekt av bookinInfo om det finns mer bord som är bokad på samma tid, t.ex. programmet felaktigt sparar bord nr 1 om det finns redan bokning till kl 18 för bord nr 1 och 2. "break;" skyddar mot liknande händelser.
+                            }
+                            else
+                            {
+                                checkIfAvailable = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (checkIfAvailable == true)
+            {
+                history.Add(customer);
+                DisplayContent();
+                MessageBox.Show($"Bokning är klart. Kundnamn är: {customer.Name}","Bekräftelse", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            Clear();
+
         }
         private void DisplayContent()
         {
             ConfirmedList.ItemsSource = null;
-            ConfirmedList.ItemsSource = history;  
+            ConfirmedList.ItemsSource = history;
         }
 
         private void Avboka_Click(object sender, RoutedEventArgs e)
         {
             if (ConfirmedList.SelectedItem == null)
-                return;           
+                return;
             history.Remove((BookingInfo)ConfirmedList.SelectedItem);
             DisplayContent();
-           
         }
+        private void Clear()
+        {
+            PickADay.SelectedDate = null;
+            TimeComboBox.ItemsSource = null;
+            TableComboBox.SelectedValue = null;
+            NameTextBox.Text = "";
+        }
+        private void BokaEnabled()
+        {
+            if (PickADay.SelectedDate == null || TimeComboBox.SelectedItem == null || TableComboBox.SelectedItem == null || NameTextBox.Text.Length == 0)
+            {
+                Book.IsEnabled = false;
+            }
+            else
+            {
+                Book.IsEnabled = true;
+            }
+        }
+
+        private void NameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BokaEnabled();
+        }
+
+        private void TableComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BokaEnabled();
+        }
+
+        private void TimeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BokaEnabled();
+        }   
+        private void StartExamplesRandom()  //metod för att lägga till ett antal bokningar redo att visas
+        {
+            string[] exampleNames = { "Anna", "Sofia", "Anders", "Magnus", "Ted" };
+            string[] hour = { "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00" };
+            while (history.Count < 20)
+            {
+                Random rnd = new Random();
+                int randomDay = rnd.Next(0, 31);
+                DateTime randomDate = DateTime.Now.AddDays(randomDay);
+                int nameIndex = rnd.Next(exampleNames.Length);
+                int hourIndex = rnd.Next(hour.Length);
+                int table = rnd.Next(1, 6);
+                var customer = new BookingInfo(randomDate.ToShortDateString(), hour[hourIndex], table.ToString(), exampleNames[nameIndex]);
+                bool checkIfAvailable = true;
+                if (history.Count > 0)
+                {
+                    foreach (var customerBookingInfo in history)
+                    {
+                        if (customer.Date == customerBookingInfo.Date)
+                        {
+                            if (customer.Time == customerBookingInfo.Time)
+                            {
+                                if (customer.TableNumber == customerBookingInfo.TableNumber)
+                                {
+                                    checkIfAvailable = false;
+                                    break;
+                                }
+                                else
+                                {
+                                    checkIfAvailable = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (checkIfAvailable == true)
+                {
+                    history.Add(customer);
+                    DisplayContent();
+                }
+            }
+        }
+
     }
 }
